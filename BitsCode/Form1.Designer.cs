41c6319664c0def28ff7c3aeb0504c82274dc2ea
patty98 <linascabeskrovnaya@gmail.com>
2018-12-13 22:26:13 +0300

    1

---
+++ BitsCode/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace BitsCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 366);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 38);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(221, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "d and k:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(570, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Inform. speed R:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(221, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "d and k:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(570, 203);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Inform. speed R:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(-3, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(167, 34);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Above the border\r\n of Varshamov-Gilbert";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(-3, 251);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(167, 34);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Below the border\r\n of Varshamov-Gilbert";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(184, 56);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(221, 122);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(513, 56);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(232, 122);
+            this.richTextBox2.TabIndex = 12;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(184, 282);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(232, 122);
+            this.richTextBox3.TabIndex = 13;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(513, 272);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(232, 122);
+            this.richTextBox4.TabIndex = 14;
+            this.richTextBox4.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+    }
+}
+
---
+++ BitsCode/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace BitsCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private string CreateMes(int num_letters)
+        {
+            Random rnd = new Random();
+            
+
+            // Создаем массив букв, которые мы будем использовать.
+            char[] letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZqwertyuiopasdfghjklzxcvbnm;'-=[]./!,".ToCharArray();
+
+            // Создаем генератор случайных чисел.
+            Random rand = new Random();
+
+                string word = "";
+                for (int j = 1; j <= num_letters; j++)
+                {
+
+                    int letter_num = rand.Next(0, letters.Length - 1);
+
+                    // Добавить письмо.
+                    word += letters[letter_num];
+                }
+            return word;
+
+            }
+        private StringBuilder Coding(string message)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in System.Text.Encoding.Unicode.GetBytes(message))
+                sb.Append(Convert.ToString(b, 2));
+
+
+            return sb;
+        }
+            private void ChekingBoard()
+            {
+            
+                double p = 0.005;
+                int d;
+                int t;
+                int num_letters = 5;
+                int n=0;
+                double result=1;
+                string message;
+                int k = 0;
+                double speed=0;
+                int counter = 3;
+                double P = 0.00001;
+
+                int counter1 = 0;
+                int counter2 = 0;
+
+            while (true)
+           {
+                message = CreateMes(num_letters);
+               // k = message.Length;
+
+                
+                StringBuilder sb = new StringBuilder();
+              
+                sb= Coding(message);
+                string bits = sb.ToString();
+                k = bits.Length;
+                if(k<200)
+                {
+                    while (k < 600)
+                    {
+                        num_letters++;
+                        message = CreateMes(num_letters);
+                        k = message.Length;
+                        sb= Coding(message);
+                        bits = sb.ToString();
+                        k = bits.Length;
+                    }
+                }
+
+                bits = CreateCode(bits,k);
+
+                char[] bits1 = bits.ToCharArray();
+                n = bits1.Length;
+                char[] bitsDist=new char[bits1.Length];
+                for (int i=0;i<bits1.Length;i++)
+                {
+                    bitsDist[i] = bits[i];
+                }
+                int r = n - k;
+
+                bitsDist = Distortion(bits1,bitsDist, n, counter);
+                d = DistanceHm(n, bits1, bitsDist);
+                
+
+                t = (d - 1) / 2;
+                result =Error(n, d, t, p);
+               
+                speed = Convert.ToDouble(k) / Convert.ToDouble(n);
+                if (result <= P)
+                {
+                    if (r >= (int)GilbertBoard(d,n))
+                    {
+                        richTextBox1.Text+= "n= " + n + " ;";
+                        richTextBox1.Text+= "k= " + k + " ;";
+                        richTextBox1.Text+= "d= " + d + " ;\n";
+                        richTextBox2.Text+= "R= " + speed + " ; \n";
+                        counter1++;
+
+                    }
+                    if (r <(int) GilbertBoard(d,n))
+                    {
+                        richTextBox3.Text += "n= " + n + " ;";
+                        richTextBox3.Text += "k= " + k + " ;";
+                        richTextBox3.Text += "d= " + d + " ;\n";
+                        richTextBox4.Text += "R= " + speed + " ; \n";
+                        counter2++;
+                    }
+
+                }
+
+
+                num_letters++;
+
+
+                counter = n / 200;
+
+                   
+                  
+                
+            }
+    }
+
+
+            private double GilbertBoard(int d,int n)
+            {
+                double res = 0;
+
+                for (int i = 0; i <(d-2); i++)
+                {
+                    res +=(double) (factorial(n-1) / (factorial(n-1 - i) * factorial(i)));
+                }
+
+
+                return Math.Log(res, 2);
+            }
+
+            static BigInteger factorial(BigInteger x) { return x <= 1 ? 1 : x * factorial(x - 1); }
+
+            private double Error(int n, int d, int t, double p)
+            {
+               double result=0;
+               double combin = 0;
+            
+
+            
+            //   for (int i = 0; i <= t; i++)
+              //  {
+                    combin = (double)(factorial(n) / (factorial(n - 1) * factorial(1)));
+                    result =Math.Pow(p, 1) * Math.Pow((1 - p), (n - 1)) * combin; 
+
+            //    }
+                return result;
+            }
+
+            private int DistanceHm(int n, char[] bits, char[] bitsDist)
+            {
+                int d = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    if (bits[i] != bitsDist[i])
+                    {
+                        d++;
+                    }
+                }
+                return d;
+            }
+
+            private char [] Distortion(char [] bits,char[] bitsDist, int n, int counter)
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < counter; i++)
+                {
+                foreach (byte b in System.Text.Encoding.Unicode.GetBytes(rnd.Next(0, 2).ToString()))
+                    bitsDist[i] = Convert.ToChar(b);
+                if (bits[i]==bitsDist[i])
+                {
+
+                    for(; ;)
+                     
+                    {
+                        byte [] b = System.Text.Encoding.Unicode.GetBytes(rnd.Next(0, 2).ToString());
+                        if (b[0] != bitsDist[i])
+                        {
+                            bitsDist[i] = Convert.ToChar(b[0]);
+                            break;
+
+                        }
+                           
+                         
+
+                    }   
+                }
+
+                }
+
+
+                return bitsDist;
+            }
+       
+        private string CreateCode(string bits, int k)
+        {
+            int pow = 0;
+            int i = 0;
+            for (i=0;;i++)
+            {
+                
+                pow=(int)Math.Pow(2, i);
+                if(pow>=k)
+                {
+                    i--;
+                    break;
+                }
+                else
+                    { string s = '0'.ToString();
+                bits = bits.Insert(pow,s); }
+               
+
+            }
+
+            return bits;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ChekingBoard();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
